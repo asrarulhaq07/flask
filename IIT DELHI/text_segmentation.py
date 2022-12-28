4a623106c2d3b5103b73669c74ba3c04c96ca789
asrarulhaq07 <121474468+asrarulhaq07@users.noreply.github.com>
2022-12-28 15:16:31 +0530

    Add files via upload

---
+++ IIT DELHI/text_segmentation.py
@@ -0,0 +1,9 @@
+# text segmentation of using unet model
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+from keras.layers import *
+from keras.models import Model
+from keras.optimizers import Adam
+import random
